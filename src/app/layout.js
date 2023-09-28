c358c6aaabec40f5c0e1df25a390d076b52f489b
Travis <travislogant@gmail.com>
2023-09-28 10:52:48 +0100

    added dark mode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,33 @@
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeContextProivder } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Embrace Neurodiversity',
-  description: 'Learn all about neurodiversity!',
-}
+  title: "Embrace Neurodiversity",
+  description: "Learn all about neurodiversity!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='container'>
-          <div className='wrapper'>
-            <Navbar/>
-              {children}
-            <Footer/>
-          </div>
-        </div>
+        <ThemeContextProivder>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProivder>
       </body>
     </html>
-  )
+  );
 }
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,35 @@
-import style from './themeToggle.module.css';
+"use client";
+
+import style from "./themeToggle.module.css";
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-    return <div className={style.container}>ThemeToggle</div>;
+  const { toggle, theme } = useContext(ThemeContext);
+
+  console.log(theme);
+
+  return (
+    <div
+      className={style.container}
+      onClick={toggle}
+      style={
+        theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }>
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div
+        className={style.ball}
+        style={
+          theme === "dark"
+            ? { left: 1, background: "#0f172a" }
+            : { right: 1, background: "white" }
+        }></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
+  );
 };
 
-export default ThemeToggle
+export default ThemeToggle;

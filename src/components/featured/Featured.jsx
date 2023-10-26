990f81c2e13cf1884c272f7928d9bacdec0626e7
Travis <travislogant@gmail.com>
2023-10-26 11:08:40 +0100

    removed Lorem text and put placeholders

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -8,18 +8,21 @@ const Featured = () => {
       <h1 className={styles.title}>
         <b>Discover Neurodiversity</b>
       </h1>
-      <span className={styles.subtitle}>Embracing Minds, Inspiring Hearts!</span>
+      <div className={styles.subtitle}>Embracing Minds, Inspiring Hearts!</div>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
-          <Image src="/Neurodiversity.jpg" alt="" fill />
+          <Image src="/Neurodiversity.png" alt="" fill />
         </div>
         <div className={styles.textContainer}>
-          <h1 className={styles.postTitle}>This a test Title</h1>
+          <h1 className={styles.postTitle}>Understand your Neurodiversity</h1>
           <p className={styles.postDescription}>
-            This is a test description. Here at Neurodiversity, we want to help
-            people feel inspired about their unique differences!
+            Welcome to this platform to help you learn about all the types and
+            the intricate differences of people who have experianced
+            Neurodiversities. We want to help people feel inspired about their
+            unique differences! As well as help people understand the
+            differences of others. How you can help and how you can be helped.
           </p>
-          <button className={styles.button}>Read More</button>
+          <button className={styles.button}>Learn More</button>
         </div>
       </div>
     </div>
